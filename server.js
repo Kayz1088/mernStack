377bc004b3c7954855e9435650e38250a176954f
kennyg8810@gmail.com <kennyg8810@gmail.com>
2018-11-10 00:39:14 -0500

    fixed issues

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ mongoose
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html' ));
-    })
+      res.sendFile(path.resolve( __dirname, 'client', 'build', 'index.html' ));
+    });
   }
 
   const port = process.env.PORT || 5000;
